42bae1227b1d81d79f7e87a0918f8bb12dc36db5
Yasin047 <95363870+Yasin047@users.noreply.github.com>
2023-08-03 00:38:05 +0600

    updated

--- app/page.js
+++ app/page.js
@@ -14,15 +14,13 @@ const Home = async ({ searchParams }) => {
   return (
     <Container>
       <div className="pt-24 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-6 gap-8">
-        {listing?.map((listing) => {
-          return (
-            <ListingCard
-              key={listing.id}
-              data={listing}
-              currentUser={currentUser}
-            />
-          );
-        })}
+        {listing?.map((listing) => (
+          <ListingCard
+            key={listing.id}
+            data={listing}
+            currentUser={currentUser}
+          />
+        ))}
       </div>
     </Container>
   );
